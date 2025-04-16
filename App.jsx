d64e547315a5f54d83b3e33d347122747ef97f44
IndanaPuspitaNingrum <a710220064@student.ums.ac.id>
2025-04-16 18:28:51 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Arif', number: '081111' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    // Cek duplikat nama
+    const isDuplicate = persons.some(person => person.name === newName)
+    if (isDuplicate) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
